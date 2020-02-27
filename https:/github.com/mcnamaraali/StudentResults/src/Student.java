967461dcbdb2d8a126ee1cf2613fab5d4f3c4c51
alimcnamara <mcnamaraali@gmail.com>
2020-02-27 12:03:00 +0100

    Student and StudentTest classes - covering Control Statements part 1

---
+++ https:/github.com/mcnamaraali/StudentResults/src/Student.java
@@ -0,0 +1,57 @@
+
+public class Student {
+	private String name;
+	private double average;
+	
+	public Student(String name, double average) {
+		this.name = name;
+		
+		if(average > 0.0 ) {
+			if(average <= 100.0) {
+				this.average = average;
+			}
+		}
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setAverage(double average) {
+		
+		if(average > 0.0 ) {
+			if(average <= 100.0) {
+				this.average = average; //assign to instance variable
+			}
+		}
+	}
+	
+	public double getAverage() {
+		return average;
+	}
+	
+	public String getLetterGrade() {
+		String letterGrade = "";
+		
+		if(average >= 70.0) {
+			letterGrade = "A";
+		}
+		else if (average >= 60.0) {
+			letterGrade = "B";
+		}
+		else if (average >= 50.0) {
+			letterGrade = "C";
+		}
+		else if (average >= 40.0) {
+			letterGrade = "D";
+		}
+		else {
+			letterGrade = "F";
+		}
+		return letterGrade;
+	}
+}
